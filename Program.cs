b6bea173c160c1992638f301e7296eab4d4f6de2
Zeiadelmekabaty <121629337+Zeiadelmekabaty@users.noreply.github.com>
2022-12-29 20:53:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Prime numbers from " + n1 + " to " + n2 + ": ");
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
+        static bool IsPrimeNumber(int n)
+        {
+           if( n == 1 || n == 0)
+            {
+                return false;
+            }
+           for ( int i = 2; i < n; i++ )
+            {
+                if (n % i == 0)
+
+                    return false;               
+            } 
+           return true;
+        }
+    }
+}
+
